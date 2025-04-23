adbc0c689c143a806e5f79737c811368924548c1
chickendrop89 <chickendrop89@gmail.com>
2025-04-24 01:37:17 +0200

    ose: add a copyleft license along with headers
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- src/args.py
+++ src/args.py
@@ -1,3 +1,16 @@
+#  OneShot-Extended (WPS penetration testing utility) is a fork of the tool with extra features
+#  Copyright (C) 2025 chickendrop89
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
 import argparse
 import os
 
